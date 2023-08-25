5a2bf16641cb6825c1dba6f9fc5ae6605d0cdaa2
Sophonie Bouye <sophoniebouye@gmail.com>
2023-08-25 18:16:43 +0000

    feat(): download node version for linux and arm64 architecture

--- sopho_nvm.sh
+++ sopho_nvm.sh
@@ -9,13 +9,13 @@ NODEJS_RELEASES="https://nodejs.org/download/release/index.json"
 NODEJS_BASE_URL="https://nodejs.org/dist/"
 NODEJS_VERSIONS_REGEX='/v(\d+)\.(\d+)\.(\d+)/'
 NODEJS_VERSIONS=
-NODEJS_LTS_VERSION=
 ARGUMENT_LIST=(
     "help"
     "lts"
     "current"
     "version"
 )
+ARCH=""
 
 ############################################################
 # Help                                                     #
@@ -35,21 +35,32 @@ get_help()
 }
 
 
-get_all_release()
+get_arch()
 {
-    VERSIONS=$($CURL -X GET $NODEJS_RELEASES -H "Content-type: application/json" -H "Accept: application/json" | jq -r '.[] | select(.lts != false) | [ .version, .date, .lts, select(.files == "linux-x64") ]')
-    echo $VERSIONS
+    case $(uname -m) in
+        x86_64)
+            ARCH="linux-x64" ;;
+        arm)
+            dpkg --print-architecture | grep -q "arm64" && ARCH="linux-arm64" || ARCH="linux-arm71" ;;
+    esac
 }
+
 ############################################################
 # LTS                                                      #
 ############################################################
 
-
-
 get_lts()
 {
+    NODEJS_LTS_VERSION=$($CURL -s -X GET $NODEJS_RELEASES -H "Content-type: application/json" -H "Accept: application/json" | jq -r 'first(.[] | select(.lts != false)) | .version')
+
+    get_arch # Get current architecture
+
     echo "Get Nodejs lts version"
-    get_all_release    
+    echo "Latest LTS version = "
+    echo 
+    echo $NODEJS_LTS_VERSION
+    NODEJS_LTS_DOWNLOAD=$($CURL -# -L -O "${NODEJS_BASE_URL}${NODEJS_LTS_VERSION}/node-${NODEJS_LTS_VERSION}-${ARCH}.tar.xz")
+    echo $NODEJS_LTS_DOWNLOAD 
 }
 
 
@@ -64,6 +75,11 @@ while true ; do
             exit 0;;
         --lts)
             get_lts
+            exit 0;;
+        *)
+            echo "Error please give valid argument"
+            echo
+            get_help
             exit 0;;
         --) shift
             break;;
